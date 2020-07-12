ccd64b0efd041e6190dc9f8a97a2bc26af835d06
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-07-12 14:17:40 +0300

    SharedDigit

---
+++ SharedDigit/src/SharedDigit.java
@@ -0,0 +1,12 @@
+public class SharedDigit {
+    public static boolean hasSharedDigit(int firstNUmber, int secondNumber){
+        if ((firstNUmber <= 10 || firstNUmber >= 99) && (secondNumber <= 10 || secondNumber >= 99)){
+            return false;
+        }
+        int firstNumberDigit = 0;
+        int secondNumberDigit = 0;
+        while ((firstNUmber != 0) && (secondNumber!=0)){
+            
+        }
+    }
+}
