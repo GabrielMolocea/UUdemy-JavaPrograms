381f528ccc95d1ae8c32b29a3a32a6df7dadccef
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-07-12 14:31:38 +0300

    SharedDigit

--- SharedDigit/src/SharedDigit.java
+++ SharedDigit/src/SharedDigit.java
@@ -3,10 +3,21 @@ public class SharedDigit {
         if ((firstNUmber <= 10 || firstNUmber >= 99) && (secondNumber <= 10 || secondNumber >= 99)){
             return false;
         }
+        int originalSecondNumber = secondNumber;
         int firstNumberDigit = 0;
         int secondNumberDigit = 0;
-        while ((firstNUmber != 0) && (secondNumber!=0)){
-            
+        while ((firstNUmber != 0) ){
+            int firstRemainder = firstNUmber % 10;
+            while (secondNumber !=0){
+                int secondRemainder = secondNumber % 10;
+                if (firstRemainder == secondRemainder){
+                    return true;
+                }
+                secondNumber /= 10;
+            }
+            secondNumber = originalSecondNumber;
+            firstNUmber /=10;
         }
+        return false;
     }
 }
