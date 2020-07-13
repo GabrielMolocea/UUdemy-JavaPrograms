b8916be71b5345f6fbf8b995d7235bd4e780d7d0
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-07-13 16:23:31 +0300

    GreatestCommonDivisor

--- GreatestCommonDivisor/src/GreatestCommonDivisor.java
+++ GreatestCommonDivisor/src/GreatestCommonDivisor.java
@@ -1,2 +1,43 @@
 public class GreatestCommonDivisor {
-}
+
+    public static int getGreatestCommonDivisor(int first,int second) {
+
+        if(first<10 || second<10) {
+
+            return -1;
+
+        }
+
+        int max = 0,gcd1=0,gcd2=0;
+
+        for(int i=1;i<=first;i++) {
+
+            if(first%i == 0) {
+
+                gcd1 = i;
+
+            }
+
+            if(second%i == 0) {
+
+                gcd2 = i;
+
+            }
+
+            if(gcd1 == gcd2) {
+
+                if(max<gcd1) {
+
+                    max = gcd1;
+
+                }
+
+            }
+
+        }
+
+        return max;
+
+    }
+
+}     
