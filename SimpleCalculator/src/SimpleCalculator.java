3c4ac700e7eb76a3768888edd8d411c5b93072ab
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-07-23 16:06:40 +0300

    SimpleCalculator

---
+++ SimpleCalculator/src/SimpleCalculator.java
@@ -0,0 +1,41 @@
+public class SimpleCalculator{
+
+    private double firstNumber;
+    private double secondNumber;
+
+    //Setters
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    //Getters
+    public double getFirstNumber(){
+        return this.firstNumber;
+    }
+    public double getSecondNumber(){
+        return this.secondNumber;
+    }
+    //Additional task
+    public double getAdditionResult(){
+        return firstNumber + secondNumber;
+    }
+
+    public double getSubtractionResult(){
+        return firstNumber - secondNumber;
+    }
+
+    public double getMultiplicationResult(){
+        return firstNumber * secondNumber;
+    }
+    public double getDivisionResult(){
+        if(secondNumber == 0){
+            return 0;
+        } else {
+            return firstNumber / secondNumber;
+        }
+    }
+}
