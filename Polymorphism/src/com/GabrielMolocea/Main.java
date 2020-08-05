558e4a40781a753de1d15af7d5d776bd78d9c10c
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-08-05 09:41:55 +0300

    Polymorphism

--- Polymorphism/src/com/GabrielMolocea/Main.java
+++ Polymorphism/src/com/GabrielMolocea/Main.java
@@ -53,17 +53,37 @@ class StarWars extends Movie{
         return "Imperial Forces try to take over the universe";
     }
 
-    class Forgetable extends Movie{
-        public Forgetable() {
-            super("Forgetable");
-        }
-        //
+
+}
+class Forgettable extends Movie{
+    public Forgettable() {
+        super("Forgetable");
     }
+    // No plot method
 }
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    public static Movie randomMovie(){
+        int randomNumber = (int) (Math.random() * 5) +1;
+        System.out.println("Random number generated was " + randomNumber);
+
+        switch (randomNumber){
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependentsDay();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWars();
+            case 5:
+                return new Forgettable();
+        }
+        return null;
     }
 }
