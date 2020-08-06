d60ef1fec16e1b7143612b1eab34b9382a035fdf
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-08-06 16:58:20 +0300

    OOP Test

--- OOPTest/src/com/GabrielMolocea/Main.java
+++ OOPTest/src/com/GabrielMolocea/Main.java
@@ -10,6 +10,12 @@ public class Main {
         hamburger.addHamburgerAddition3("Cheese",1.12);
         System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
 
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon",5.67);
+        healthyBurger.addHealthAddition1("Egg",5.43);
+        healthyBurger.addHealthAddition2("Lentils",3.41);
+        System.out.println("Total Healthy Burger price is " + healthyBurger.itemizeHamburger());
 
+        DeluxBurger deluxBurger = new DeluxBurger();
+        deluxBurger.itemizeHamburger();
     }
 }
