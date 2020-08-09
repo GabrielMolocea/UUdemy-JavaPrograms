e3cacf2e3ae7780668ba94e0fe1b0d3847aef7ea
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-08-09 10:45:05 +0300

    References Vs Values Types

--- ReferencesVsValuesTypes/src/com/GabrielMolocea/Main.java
+++ ReferencesVsValuesTypes/src/com/GabrielMolocea/Main.java
@@ -1,5 +1,7 @@
 package com.GabrielMolocea;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -17,5 +19,12 @@ public class Main {
 
     int[] myIntArray = new int[5];
     int[] anotherArray = myIntArray;
+        System.out.println("myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("anotherArray = " + Arrays.toString(anotherArray));
+
+    anotherArray[0] = 1;
+        System.out.println("after change myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("after change anotherArray = " + Arrays.toString(anotherArray));
+
     }
 }
