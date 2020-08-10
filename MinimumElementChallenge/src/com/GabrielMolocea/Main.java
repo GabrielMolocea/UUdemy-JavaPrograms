9d23eef0f4793956e24dcaf72c46efb78a1ce159
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-08-10 17:21:16 +0300

    Minimum Element Challenge

--- MinimumElementChallenge/src/com/GabrielMolocea/Main.java
+++ MinimumElementChallenge/src/com/GabrielMolocea/Main.java
@@ -6,10 +6,17 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Enter count");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+        int[] returnedArray = redIntegers(count);
+
+        int returnedMin = findMin(returnedArray);
+        System.out.println("The min value in the array " + returnedMin);
     }
 
-    public int[] redIntegers(int count){
+    public static int[] redIntegers(int count){
         int[] array = new int[count];
         for (int i =0; i < array.length; i++){
             System.out.println("Enter a number:\r");
@@ -18,5 +25,17 @@ public class Main {
             array[i] = num;
         }
         return array;
+    }
+
+    private static int findMin(int[] array){
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++){
+            int value = array[i];
+
+            if (value < min){
+                min = value;
+            }
+        }
+        return min;
     }
 }
