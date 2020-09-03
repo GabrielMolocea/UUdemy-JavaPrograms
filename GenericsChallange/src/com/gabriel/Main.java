31a21ced6a66fd88d5f23770b0da117a80ff78c0
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-09-03 11:31:50 +0300

    Sports League that can only access the specific sport class

--- GenericsChallange/src/com/gabriel/Main.java
+++ GenericsChallange/src/com/gabriel/Main.java
@@ -4,6 +4,7 @@ public class Main {
     
     public static void main(String[] args) {
         League<Team<FootbalPlayer>> footballLeague = new League<>("AFL");
+        League<Team<BaseballPlayer>> baseballLeague = new League<>("ANS");
         
         Team<FootbalPlayer> adelaideCrows = new Team<>("Adelaide Crows");
         Team<FootbalPlayer> melbourne = new Team<>("Melbourne");
@@ -18,8 +19,17 @@ public class Main {
         hawthorn.matchResult(fremantle,1,0);
         hawthorn.matchResult(adelaideCrows,3,0);
         adelaideCrows.matchResult(fremantle,2,1);
-        
+    
         footballLeague.showLeagueTable();
+    
+        //Baseball League
+        Team<BaseballPlayer> readDolphin = new Team<>("ReadDolphin");
+        Team<BaseballPlayer> lazyMice = new Team<>("LayMice");
+        
+        baseballLeague.add(readDolphin);
+        baseballLeague.add(lazyMice);
+    
+    
+    
     }
-
 }
