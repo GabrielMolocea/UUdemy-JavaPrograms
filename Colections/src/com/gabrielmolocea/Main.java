befffea1bfb0ec415a644506fbcaba8453b156b9
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-09-13 14:37:01 +0300

    A Theatre with reserving call,canceling and showing all seats in Theatre

--- Colections/src/com/gabrielmolocea/Main.java
+++ Colections/src/com/gabrielmolocea/Main.java
@@ -4,13 +4,13 @@ public class Main {
     
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Olymoim", 8, 12);
-//        theatre.getSeats();
+//       theatre.getSeats();
         if (theatre.reserveSeat("H11")) {
             System.out.println("Please pay");
         } else {
             System.out.println("Sorry, seat is taken");
         }
-    
+
         if (theatre.reserveSeat("H11")) {
             System.out.println("Please pay");
         } else {
