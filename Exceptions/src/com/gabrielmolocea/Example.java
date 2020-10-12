6c98aaa4a287513c82a28b1813343a35e208beb0
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-10-12 14:30:28 +0300

    Exceptions

--- Exceptions/src/com/gabrielmolocea/Example.java
+++ Exceptions/src/com/gabrielmolocea/Example.java
@@ -9,14 +9,33 @@ public class Example {
         System.out.println(result);
     }
     private static int divide() {
-        int x = getInt();
-        int y = getInt();
+        int x, y;
+        try {
+            x = getInt();
+            y = getInt();
+        } catch (NoSuchElementException exception) {
+            throw  new ArithmeticException("no suitable input");
+        }
+        
         System.out.println("x is: " + x + ", y is: " + y);
-        return x / y;
+        try {
+            return x / y;
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("attempt to divide by 0");
+        }
     }
     private static int getInt() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a number: ");
-        return scanner.nextInt();
+        while (true) {
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException exception) {
+                //go round again. Read past the end of line in the input first
+                scanner.nextLine();
+                System.out.println("Please enter a number only using digits form 0 to 9");
+            }
+        }
+
     }
 }
