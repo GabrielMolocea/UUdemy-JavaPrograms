f577a8a4d5c29febea6d8b64929ba32a2568682e
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-10-13 13:34:33 +0300

    Input Output

--- InputOutput/src/com/gabrielmolocea/Main.java
+++ InputOutput/src/com/gabrielmolocea/Main.java
@@ -9,9 +9,7 @@ public class Main {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        
-    
+//No more Labyrinth in Main Class
         Map<String,String> vocabulary = new HashMap<>();
         vocabulary.put("QUIT","Q");
         vocabulary.put("NORTH","N");
