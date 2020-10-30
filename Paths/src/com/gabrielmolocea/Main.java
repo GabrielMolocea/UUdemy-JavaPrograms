ce0f61176994b0ca1977dc6e9fbe7300395c9320
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-10-30 12:31:36 +0200

    Accessing Files in WorkingDirectory and OutSide of it.

--- Paths/src/com/gabrielmolocea/Main.java
+++ Paths/src/com/gabrielmolocea/Main.java
@@ -4,43 +4,89 @@ import java.io.*;
 import java.nio.file.*;
 
 public class Main {
-
-    public static void main(String[] args) {
-	   
-        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
-        printFile(path);
-        System.out.println("\n");
-        
-        //Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
-        
-        //Order of the arguments are very important -> Wrong order of the arguments ca cause to show like:
-        //SubdirectoryFile.txt\\files for example.
-        Path filePath = Paths.get(".","files","SubdirectoryFile.txt");
-        printFile(filePath);
-        
-        System.out.println("\n");
-        filePath = Paths.get("F:\\Udemy-JavaPrograms\\UUdemy-JavaPrograms\\OutThere.txt");
-        //Example of how to write absolute path different and get the same result
-        //filePath = Paths.get("D:\\", "Examples", "OutThere.txt");
-        printFile(filePath);
-        
-        // Getting the absolut path
-        filePath = Paths.get(".");
-        System.out.println(filePath.toAbsolutePath());
-        Path path2 = FileSystems.getDefault().getPath(".","files","..","files","SubdirectoryFile.txt");
-        System.out.println(path2.normalize().toAbsolutePath());
-        printFile(path2.normalize());
-    }
     
-    private static void printFile(Path path) {
-        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
-            String line;
-            while ((line = fileReader.readLine()) != null) {
-                System.out.println(line);
-            }
+    public static void main(String[] args) {
+        try {
+            
+//            //Copy File
+//            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+//
+//            // Copy Directory
+//            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
+//            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
+//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+//
+//            // Moving files
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples","file1copy.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+//            Files.move(fileToMove, destination);
+//
+//            //Renaming Files
+//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Path destination = FileSystems.getDefault().getPath("Examples", "file1.txt");
+//            Files.move(fileToMove,destination);
+        
+            // Deleting Files
+            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+            Files.deleteIfExists(fileToDelete);
+            
         } catch (IOException e) {
             System.out.println(e.getMessage());
-            e.printStackTrace();
         }
+        
+
+        
     }
 }
+//        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+//        printFile(path);
+//        System.out.println("\n");
+//
+//        //Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
+//
+//        //Order of the arguments are very important -> Wrong order of the arguments ca cause to show like:
+//        //SubdirectoryFile.txt\\files for example.
+//        Path filePath = Paths.get(".","files","SubdirectoryFile.txt");
+//        printFile(filePath);
+//
+//        System.out.println("\n");
+//        filePath = Paths.get("F:\\Udemy-JavaPrograms\\UUdemy-JavaPrograms\\OutThere.txt");
+//        //Example of how to write absolute path different and get the same result
+//        //filePath = Paths.get("D:\\", "Examples", "OutThere.txt");
+//        printFile(filePath);
+//
+//        // Getting the absolut path
+//        filePath = Paths.get(".");
+//        System.out.println(filePath.toAbsolutePath());
+//        Path path2 = FileSystems.getDefault().getPath(".","files","..","files","SubdirectoryFile.txt");
+//        System.out.println(path2.normalize().toAbsolutePath());
+//        printFile(path2.normalize());
+//
+//        Path path3  = FileSystems.getDefault().getPath("thisfiledosentexit.txt");
+//        System.out.println(path3.toAbsolutePath());
+//
+//        Path path4 = Paths.get("Z:\\", "abdef", "whatever.txt");
+//        System.out.println(path4.toAbsolutePath());
+//
+//        filePath = FileSystems.getDefault().getPath("files");
+//        System.out.println("Exits = " + Files.exists(filePath));
+//
+//        System.out.println("exits = " + Files.exists(path4));
+//
+//
+//    }
+//
+//    private static void printFile(Path path) {
+//        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
+//            String line;
+//            while ((line = fileReader.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//        }
+//    }
+
