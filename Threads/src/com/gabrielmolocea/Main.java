50d3fd554ceb45e29a7674b4dd25c8018bb98f47
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-11-04 10:33:21 +0200

    Creating a Thread and a subThread

--- Threads/src/com/gabrielmolocea/Main.java
+++ Threads/src/com/gabrielmolocea/Main.java
@@ -11,19 +11,24 @@ public class Main {
         anotherThread.setName("---- Another Thread ----");
         anotherThread.start();
         
-        new Thread() {
-            public void run() {
-                System.out.println(ANSI_GREEN + "Hello from the anonymous class thread");
-            }
-        }.start();
+        new Thread(() ->
+                System.out.println(ANSI_GREEN + "Hello from the anonymous class thread")).start();
         
         Thread myRunnableThread = new Thread(new MyRunnable() {
             @Override
             public void run() {
                 System.out.println(ANSI_RED + "Hello form anonymous class`s implementation of run()");
+                try {
+                    anotherThread.join();
+                    System.out.println(ANSI_RED + "AnotherTread terminated, or timed out, so I`m running again");
+                } catch (InterruptedException e) {
+                    System.out.println(ANSI_RED + "I couldn't wait after all. I was interrupted");
+                    
+                }
             }
         });
         myRunnableThread.start();
+        //anotherThread.interrupt();
         
         System.out.println(ANSI_PURPLE + "Hello again form the Main thread");
     
